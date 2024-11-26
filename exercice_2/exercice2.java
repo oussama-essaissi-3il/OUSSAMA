d764aa93b380da706f5d70456bd6c9f37cd6c41f
oussama essaissi <essaisso@3il.fr>
2024-11-26 16:11:30 +0100

    Correction exercice 2 : amélioration du code

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,31 @@
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
-    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+    /**
+     * Affiche les notes et la moyenne d'un étudiant
+     */ 
+     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
+        afficherNotes(nomEtudiant, notes);
+        double moyenne = calculerMoyenne(notes);
+        System.out.println("Moyenne : " + moyenne);
+    }
+
+    /**
+     * Affiche les notes d'un étudiant
+     */
+    private void afficherNotes(String nomEtudiant, int[] notes) {
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
-
-        // Calcul de la somme des notes
+    }
+    /**
+     * Calcule la moyenne des notes
+     */
+    private double calculerMoyenne(int[] notes) {
         int somme = 0;
         for (int note : notes) {
             somme += note;
         }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+        return (double) somme / notes.length;
     }
 }
